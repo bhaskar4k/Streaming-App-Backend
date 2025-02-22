cbd95f213325cbba59d389d6a9720a88499409af
Bhaskar Nandy <bhaskarndy@gmail.com>
2025-02-22 21:44:07 +0530

    Prepared the dashboard microservice

--- dashboard/src/main/java/com/app/dashboard/controller/DashboardController.java
+++ dashboard/src/main/java/com/app/dashboard/controller/DashboardController.java
@@ -1,5 +1,8 @@
 package com.app.dashboard.controller;
 
+import com.app.dashboard.entity.TLogExceptions;
+import com.app.dashboard.service.LogExceptionsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +10,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/dashboard")
 public class DashboardController {
+    @Autowired
+    private LogExceptionsService logExceptionsService;
 
     @GetMapping("/menu")
     public String menu(){
+        log("upload()","e.getMessage()");
         return "Baler menu le bara";
+    }
+
+
+    private void log(String function_name, String exception_msg){
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+
+        String full_class_path = stackTraceElements[2].getClassName();
+        String class_name = full_class_path.substring(full_class_path.lastIndexOf(".") + 1);
+
+        String full_package_path = full_class_path.substring(0, full_class_path.lastIndexOf("."));
+        String package_name = full_package_path.substring(full_package_path.lastIndexOf(".") + 1);
+
+        logExceptionsService.saveLogException(new TLogExceptions(package_name,class_name,function_name,exception_msg,0L));
     }
 }
