05a03f79d8a84097ce98c893ee827a4ded057d1a
Bhaskar Nandy <bhaskarndy@gmail.com>
2025-03-20 11:36:02 +0530

    Changed JWT expire time

--- authentication/src/main/java/com/app/authentication/environment/Environment.java
+++ authentication/src/main/java/com/app/authentication/environment/Environment.java
@@ -11,8 +11,7 @@ public class Environment {
     private List<String> allowedOrigins = Arrays.asList("http://localhost:5173");
     private Long maximumLoginDevice = 2L;
     private String secretKey = "yg45eg-g56yw4r-hj45g-fy6awe-g54fw";
-//    private Long jwtExpireTime = (1000L * 60L * 60L * 10L * 300L);
-    private Long jwtExpireTime = (60000L);
+    private Long jwtExpireTime = (1000L * 60L * 60L * 10L);
 
     public String getPrivateKey1() {
         return this.privateKey1;
