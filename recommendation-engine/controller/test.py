bb2008f33066e4d91db4b144422e8a7b0d5e76f6
bhaskar4k <bhaskarndy@gmail.com>
2025-07-25 01:46:12 +0530

    Seperated controller from main.py

---
+++ recommendation-engine/controller/test.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter
+from pydantic import BaseModel
+from typing import List
+
+router = APIRouter()
+
+class Test(BaseModel):
+    name: str
+    age: int
+    friends: List[str]
+
+@router.get("/hello_python")
+def read_root():
+    test_app: List[Test] = []
+
+    test_app.append(Test(name="Bhaskar 1", age=25, friends=["Test 1", "Test 2"]))
+    test_app.append(Test(name="Bhaskar 2", age=25, friends=["Test 3", "Test 4"]))
+
+    return test_app
--- recommendation-engine/main.py
+++ recommendation-engine/main.py
@@ -1,20 +1,7 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-from typing import List
+from controller import test
 
 app = FastAPI()
 
-class Test(BaseModel):
-    name: str
-    age: int
-    friends: List[str]
-
-
-
-@app.get("/hello_python")
-def read_root():
-    test_app: List[Test] = []
-
-    test_app.append(Test(name="Bhaskar 1", age=25, friends=["Test 1, Test 2"]))
-    test_app.append(Test(name="Bhaskar 2", age=25, friends=["Test 3, Test 3"]))
-    return test_app
+# Register router
+app.include_router(test.router)
